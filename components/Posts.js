a6fba23db7a95d2c6d02791b0f43c8e61d16bb4f
lan <ngoclanpbc@gmail.com>
2022-05-11 14:25:43 +0700

    building the posts component

--- components/Posts.js
+++ components/Posts.js
@@ -1,41 +1,58 @@
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { cloneElement, useEffect, useState } from "react";
 import Post from "./Post";
-const posts = [
-    {
-        id: '1231',
-        username: 'lan',
-        userImg: 'https://bom.so/XSeNRm',
-        img: 'https://bom.so/9Yr3kx',
-        caption: 'like'
+import { db } from "../firebase";
+import { Snapshot } from "recoil";
 
-    },
-    {
-        id: '2',
-        username: 'lan',
-        userImg: 'https://avatars.githubusercontent.com/u/24712956?v=4',
-        img: 'https://bom.so/9Yr3kx',
-        caption: 'like'
+// const posts = [
+//     {
+//         id: '1231',
+//         username: 'lan',
+//         userImg: 'https://bom.so/XSeNRm',
+//         img: 'https://bom.so/9Yr3kx',
+//         caption: 'like'
 
-    },
-    {
-        id: '3',
-        username: 'lan',
-        userImg: 'https://bom.so/RtpQYz',
-        img: 'https://bom.so/9Yr3kx',
-        caption: 'like'
+//     },
+//     {
+//         id: '2',
+//         username: 'lan',
+//         userImg: 'https://avatars.githubusercontent.com/u/24712956?v=4',
+//         img: 'https://bom.so/9Yr3kx',
+//         caption: 'like'
 
-    },
-]
+//     },
+//     {
+//         id: '3',
+//         username: 'lan',
+//         userImg: 'https://bom.so/RtpQYz',
+//         img: 'https://bom.so/9Yr3kx',
+//         caption: 'like'
+
+//     },
+// ]
 function Posts() {
+    const [posts,setPosts] = useState([]);
+
+    useEffect(
+        () =>  onSnapshot(query(collection(db, 'posts'),orderBy('timestamp','desc')),
+         snapshot => {
+            setPosts(snapshot.docs);
+        })
+        
+
+    , [db]);
+
+    console.log(posts);
     return (
         <div>
             {posts.map((post) =>(
                 <Post 
                 key={post.id} 
                 id= {post.id}
-                username= {post.username}
-                userImg= {post.userImg}
-                img ={post.img}
-                caption = {post.caption}
+                username= {post.data().username}
+                userImg= {post.data().profileImg}
+                img ={post.data().image}
+                caption = {post.data().caption}
                 />
             ))}
             
